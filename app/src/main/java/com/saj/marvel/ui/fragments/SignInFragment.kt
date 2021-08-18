f9ea1042b9ab8c83fbb546f43010880bc4b7fd2d
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2021-08-18 14:46:22 -0300

    add firebase-ui

--- app/src/main/java/com/saj/marvel/ui/fragments/SignInFragment.kt
+++ app/src/main/java/com/saj/marvel/ui/fragments/SignInFragment.kt
@@ -1,10 +1,17 @@
 package com.saj.marvel.ui.fragments
 
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.saj.marvel.R
 import com.saj.marvel.databinding.FragmentSignInBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +25,39 @@ class SignInFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
         _binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            navigateToCharactersList()
+        } else {
+            Toast.makeText(requireActivity(), "ERROR: ${response?.error?.errorCode}",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToCharactersList() {
+        findNavController().navigate(R.id.charactersListFragment)
     }
 
     override fun onDestroyView() {
