5e80fe0e583973f57bf61221fa152105361f529b
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2021-08-18 12:59:53 -0300

    add SignInFragment

--- app/src/main/java/com/saj/marvel/ui/HomeActivity.kt
+++ app/src/main/java/com/saj/marvel/ui/HomeActivity.kt
@@ -1,15 +1,40 @@
 package com.saj.marvel.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.saj.marvel.R
+import com.saj.marvel.databinding.ActivityHomeBinding
+import com.saj.marvel.viewModels.AuthStateViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
+    private val authStateViewModel: AuthStateViewModel by viewModels()
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        authStateViewModel.isLoggedInLiveData.observe(this, {
+            setNavigation()
+        })
+    }
+
+    private fun setNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val navGraph = inflater.inflate(R.navigation.nav_graph)
+
+        val destination = R.id.signInFragment
+        navGraph.startDestination = destination
+        navHostFragment.navController.graph = navGraph
     }
 }
